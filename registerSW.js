a464306ecc748e24c3887df35ef2eb3d95b36db3
Foldit <Foldit@users.noreply.github.com>
2026-06-15 13:30:35 +0000

    deploy: 4c608e34af90b5eb99c6c4cf436ee34dc1b6c306

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Markdown-Viewer-PWA/sw.js', { scope: '/Markdown-Viewer-PWA/' })})}
